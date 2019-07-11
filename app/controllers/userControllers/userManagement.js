5a0d28cd22ecb5e76c798a8502db1a7aec06759b
Nicholas Kimuli <nicholaskimuli@gmail.com>
2019-07-11 15:12:18 +0300

    Enabled CORS

--- app/controllers/userControllers/userManagement.js
+++ app/controllers/userControllers/userManagement.js
@@ -1,5 +1,14 @@
 const userManagementController = require('../../models/userModels/userManagementModel')
 
 // Create new user(under a business) - Tie them down to a user group(can be custom to that business)
+exports.createUser = function(req, res) {
+    // Get params
+    let firstName = req.body['firstName']
+    let lastName = req.body['lastName']
+
+}
 
 // Create new user category - set up permissions and user group details
+exports.createCategory = function(req, res) {
+
+}
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,16 @@
-var express = require('express');
-var router = express.Router();
-var userController = require('../app/controllers/userControllers/userController');
+var express = require('express')
+var router = express.Router()
+var userController = require('../app/controllers/userControllers/userController')
+const userManagement = require('../app/controllers/userControllers/userManagement')
 
+// User Account Management - Global
 router.post('/v1/users/login', userController.login);
 router.post('/v1/users/signup', userController.signup);
 router.post('/v1/users/activate', userController.activate);
 router.post('/v1/users/reset/initiate', userController.resetInitiate);
 router.post('/v1/users/reset/complete', userController.resetComplete);
-router.post('/v1/users/create')
+
+// User Management Routes - Business
+router.post('/v1/b/users/create', userManagement.createUser)
 
 module.exports = router;
