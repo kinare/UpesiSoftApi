37958850efe4a13cee24a5b52edd4753fa057f18
Nicholas Kimuli <nicholaskimuli@gmail.com>
2019-07-23 16:55:55 +0300

    Removed unused variable

--- app/controllers/orderControllers/orderController.js
+++ app/controllers/orderControllers/orderController.js
@@ -42,8 +42,6 @@ exports.new = function(req, res) {
         })
     }
 
-    let orderItemCount = orderItems.length
-
     let orderDetails = {
         businessId: businessId,
         userId: userId,
